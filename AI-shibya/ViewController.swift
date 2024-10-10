2f9aaafe3e7df86e314c792cefa56e75048638f2
竹森雷我 <takemoriraiga@ip-10-94-2-104.ap-northeast-1.compute.internal>
2024-10-10 15:09:31 +0900

    Add

---
+++ AI-shibya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI-shibya
+//
+//  Created by 竹森雷我 on 2024/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AI-shibyaTests/AI_shibyaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_shibyaTests.swift
+//  AI-shibyaTests
+//
+//  Created by 竹森雷我 on 2024/10/10.
+//
+
+import Testing
+@testable import AI_shibya
+
+struct AI_shibyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
